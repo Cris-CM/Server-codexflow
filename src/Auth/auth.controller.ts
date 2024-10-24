c09ced805c8925d89c623cbbe58be48b5d2d84bf
LALO611adad <166889169+Cris-CM@users.noreply.github.com>
2024-10-24 01:48:12 -0500

    Feat: Agreado Api de google para Auth

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -1,25 +1,54 @@
 // src/auth/auth.controller.ts
-import { Controller, Post, Body, UsePipes, ValidationPipe, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('login')
-    @UsePipes(new ValidationPipe())
-    async login(@Body() loginDto: LoginDto): Promise<{ message: string; age: number }> { // Cambiado a objeto JSON
-        return this.authService.login(loginDto);
-    }
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  async register(
+    @Body() registerDto: RegisterDto,
+  ): Promise<{ message: string }> {
+    return this.authService.register(registerDto);
+  }
 
-    @Get('last-login') // Nuevo endpoint para obtener los datos del último inicio de sesión
-    getLastLoginData(): LoginDto | null {
-        return this.authService.getLastLoginData();
-    }
+  @Get('register')
+  getAllRegisteredUsers(): RegisterDto[] {
+    return this.authService.getAllRegisteredUsers();
+  }
 
-    @Get('login-attempts') // Nuevo endpoint para obtener todos los intentos de inicio de sesión
-    getAllLoginAttempts(): LoginDto[] {
-        return this.authService.getAllLoginAttempts();
-    }
+  @Post('login')
+  async login(
+    @Body() loginDto: LoginDto,
+  ): Promise<{ message: string; userName: string }> {
+    const user = await this.authService.login(loginDto);
+    return { message: '¡Inicio de sesión exitoso!', userName: user.nombre };
+  }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {}
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return {
+      message: 'User information from Google',
+      user: req.user,
+    };
+  }
 }
