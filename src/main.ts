3edd202a658965208f2ee6f45370f42deea27f76
LALO611adad <166889169+Cris-CM@users.noreply.github.com>
2024-11-02 18:19:15 -0500

    Style: conexion por server sin ip

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Habilitar CORS
-  await app.listen(3000, '0.0.0.0'); // Escuchar en todas las interfaces
+  app.enableCors();
+  await app.listen(3000);
 }
 bootstrap();
